0158047b6030aff39a3db0f3cf58db183d45e0d1
rysterman34 <ryanpapetti@gmail.com>
2022-01-02 13:59:35 -0700

    created boilerplate to explore data

---
+++ scripts/data_exploration.py
@@ -0,0 +1,29 @@
+'''
+data_exploration.py
+
+January 2, 2022
+
+this script does basic data exploration prior to modelling
+
+'''
+
+
+def load_data():
+    pass
+
+
+def summarize_data():
+    pass
+
+
+def plot_numerical_correlations():
+    pass
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
