6c9cc0a434093eb73a8a52bc557a894090d62538
rysterman34 <ryanpapetti@gmail.com>
2022-01-03 11:23:19 -0700

    added plotting functionality and renamed og script

--- scripts/data_exploration.py
+++ scripts/data_exploration.py
@@ -6,7 +6,7 @@ January 2, 2022
 this script does basic data exploration prior to modelling
 
 '''
-import pandas as pd, seaborn as sns, matplotlib.pyplot as plt
+import pandas as pd, seaborn as sns
 
 def load_data(data_type):
     features = pd.read_pickle(f'../data/{data_type}/features.pkl')
@@ -19,11 +19,13 @@ def visualize_numerical_data(desired_features, desired_targets):
     plottable_features = {'suicides', 'headshots', 'scorePerMinute', 'deaths', 'percentTimeMoving', 'longestStreak', 'timePlayed', 'accuracy', 'kdRatio', 'kills', 'assists', 'result'}
     plottable_data = combined_data[plottable_features]
     grid = sns.pairplot(plottable_data, corner=True, hue='result')
+    grid.fig.savefig('../results/numerical_data_visual.png',dpi=200, bbox_inches='tight')
 
 
 
 def main():
     features,targets = load_data(data_type='training')
+    visualize_numerical_data(features,targets)
 
 
 if __name__ == '__main__':
